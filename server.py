4ee158ef8477310dc8ebd5b83e008b2fc86f47fa
shivank98 <shivankgautam98@gmail.com>
2019-01-20 01:07:19 +0530

    fixes #28 and fixes #11

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 from bottle import Bottle, run, template, static_file, request, route
 import os
 import sys
+import datetime
 path = os.path.abspath(__file__)
 dir_path = os.path.dirname(path)
-
 app = Bottle()
 
+no_of_attempts = 0
+user_attempt_history = []
 with open('./files/expected_outputs/expected_output.txt', 'r') as fl:
-    expected = fl.read()
+   expected = fl.read()
+
 
 @app.get('/question/<number>')
 def question1(number):
@@ -15,22 +18,28 @@ def question1(number):
 
 @app.get('/question/<number>/download/')
 def download(number):
-    input_file_name = 'inputs'+number+'.txt'
-    return static_file(input_file_name, root=dir_path+'/'+'files/questions/')
+    input_file_name = 'inputs.txt'
+    return static_file(input_file_name, root=dir_path+'/'+'files/questions/'+str(number)+'/')
 
-@app.route('/upload', method = 'POST')
-def file_upload():
+@app.route('/question/<number>/upload/', method = 'POST') # method = 'POST'
+def file_upload(number):
     uploaded = request.files.get('upload').file.read() #uploaded outputs by user
-    with open('./files/expected_outputs/expected_output.txt', 'r') as fl:
-        expected = fl.read()
+
+    current_time = datetime.datetime.now()
+    global expected
+    global no_of_attempts
+    global user_attempt_history
     expected = expected.strip()
     uploaded = uploaded.strip()
     ans = (uploaded==expected)
-
+    
     if not ans:
+        no_of_attempts += 1
+        user_attempt_history.append(['name', number, no_of_attempts, current_time])
         return "Wrong Answer!!"
     else:
-        return "Solved! Great Job!"
+        user_attempt_history.append(['name', number, no_of_attempts, current_time])
+        return "Solved! Great Job! "
 
 
 run(app, host='localhost', port=8080)
