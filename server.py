43b74b15f47e35a30b3ca9be9eda99fd932ce7e0
shivank98 <shivankgautam98@gmail.com>
2019-01-22 23:33:51 +0530

    fixes #29

--- server.py
+++ server.py
@@ -6,22 +6,29 @@ dir_path = os.path.dirname(path)
 
 app = Bottle()
 
-with open('./files/expected_outputs/expected_output.txt', 'r') as fl:
-    expected = fl.read()
+#with open('./files/expected_outputs/expected_output.txt', 'r') as fl:
+   # expected = fl.read()
 
 @app.get('/question/<number>')
 def question1(number):
-    return static_file('index.html', root=dir_path)
+    file_path = './files/questions/'+ str(number) + '/statement.txt'
+    with open(file_path, 'r') as inputs:
+        ques = inputs.read()
+    download_link = '/question/' + str(number)+ '/download/'
+    return template('question_template.tpl', question = ques, link_to_download = download_link, number = number) # root=dir_path+'/'+'views'
 
 @app.get('/question/<number>/download/')
 def download(number):
-    input_file_name = 'inputs'+number+'.txt'
-    return static_file(input_file_name, root=dir_path+'/'+'files/questions/')
+    input_file_name = 'inputs.txt'
+    return static_file(input_file_name, root=dir_path+'/'+'files/questions/'+str(number)+'/')
 
-@app.route('/upload', method = 'POST')
-def file_upload():
+@app.route('/question/<number>/upload', method = 'POST')
+def file_upload(number):
+    file_path = './files/questions/'+ str(number) + '/output.txt'
+    with open(file_path, 'r') as fl:
+        expected = fl.read()
     uploaded = request.files.get('upload').file.read() #uploaded outputs by user
-    global expected
+    
     expected = expected.strip()
     uploaded = uploaded.strip()
     ans = (uploaded==expected)
