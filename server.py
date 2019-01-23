c6d562fe39ad455a27a20a4d98de420762e26e90
Arnab Sinha <sarnab433@gmail.com>
2019-01-23 22:26:05 +0530

    Update server.py for issue #26
    Added dictionary to store usernames and their timestamp at which they submitted their output

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
+import requests
 from bottle import Bottle, run, template, static_file, request, route
 import os
 import sys
+import datetime
 path = os.path.abspath(__file__)
 dir_path = os.path.dirname(path)
 
 app = Bottle()
+
+usernames = {}  #dictionary for storing the usernames
 
 with open('./files/expected_outputs/expected_output.txt', 'r') as fl:
     expected = fl.read()
@@ -21,6 +25,18 @@ def download(number):
 @app.route('/upload', method = 'POST')
 def file_upload():
     uploaded = request.files.get('upload').file.read() #uploaded outputs by user
+    u_name = request.forms.get('username') #accepting username
+
+    # storing usernames' submission along with the timestamp at which they submit it
+    global usernames
+    time = datetime.datetime.now()
+    t = time.strftime("%x") + ' ' + time.strftime("%I") + ":" + time.strftime("%M") + ":" + \
+        time.strftime("%S") + " " + time.strftime("%p")
+    if u_name not in usernames.keys():
+        usernames[u_name] = [(uploaded, t)]
+    else:
+        usernames[u_name].append((uploaded,t))
+
     global expected
     expected = expected.strip()
     uploaded = uploaded.strip()
