842ebea94c29cc013d26fee9cb698554c540b918
Arjoonn Sharma <arjoonn.94@gmail.com>
2019-02-04 13:34:43 +0530

    simple tests

--- test_server.py
+++ test_server.py
@@ -9,18 +9,18 @@ def test_sanity():
     assert 1 == 1, "This should never fail"
 
 
-def test_question_statement():
+def test_question_statement_can_be_obtained():
     r = R.get(api("/question/1"))
     assert r.status_code == 200
     assert "Find The Sum" in r.text
 
 
-def test_question_input_file():
+def test_question_input_file_is_available():
     r = R.get(api("/question/1/inputs.txt"))
     assert r.status_code == 200
 
 
-def test_question_submission():
+def test_question_submission_fails_for_wrong_answer():
     r = R.post(
         api("/check/1"),
         json={"username": "chacha chaudhary"},
@@ -28,3 +28,15 @@ def test_question_submission():
     )
     assert r.status_code == 200
     assert "wrong" in r.text.lower()
+
+
+def test_question_submission_passes_for_correct_answer():
+    path = "files/questions/1/output.txt"
+    with open(path, "rb") as output_file:
+        r = R.post(
+            api("/check/1"),
+            json={"username": "chacha chaudhary"},
+            files={"upload": output_file},
+        )
+    assert r.status_code == 200
+    assert "solved" in r.text.lower()
