1a15edd5f0297d1939781ba2eb0e1c66195148aa
shivank98 <shivankgautam98@gmail.com>
2019-04-10 14:38:22 +0530

    Finally woking, i was very fool

--- test_server.py
+++ test_server.py
@@ -3,6 +3,7 @@ import pytest
 #import server as app
 import requests
 
+
 url = 'http://localhost:8080'
 
 def test_question():
@@ -10,11 +11,16 @@ def test_question():
     assert r.status_code == 200
 
 def test_download():
+    #time = datetime.datetime.now()
+
+    # type(uploaded) == <class 'bytes'>
+    # uploaded outputs by user
     assert requests.get(url+'/question/1/inputs.txt').status_code == 200
 
 def test_file_upload():
-    files= open('files/questions/1/output.txt')
+    files= {'upload' : open('files/questions/1/output.txt', 'rb')}
     global url
-    url += '/check/1/output.txt'
+    url += '/check/1'
+    assert requests.post(url, files = files).status_code == 200
 
-    assert requests.post(url, files = files).status_code == 200   # givin a error
+  
