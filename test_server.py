8a04df46258bd051a290494ec2b0f9702b4338e9
Shivank Gautam <shivankgautam98@gmail.com>
2019-04-12 15:23:31 +0530

    added Test_server.py with error in 3 (#97)
    * added Test_server.py with error in 3
    
    * updated test_file_upload()
    
    * Finally woking, i was very fool
    
    * Update test_server.py
    
    Sure,
    
    Co-Authored-By: Shivank98 <shivankgautam98@gmail.com>
    
    * running like a charm and closes #43

---
+++ test_server.py
@@ -0,0 +1,28 @@
+import unittest
+import pytest
+#import server as app
+import requests
+
+
+url = 'http://localhost:8080'
+
+def test_question():
+    r = requests.get(url+'/question/1')
+    assert r.status_code == 200
+
+def test_download():
+    #time = datetime.datetime.now()
+
+    # type(uploaded) == <class 'bytes'>
+    # uploaded outputs by user
+    assert requests.get(url+'/question/1/inputs.txt').status_code == 200
+
+def test_file_upload():
+    files= {'upload' : open('files/questions/1/output.txt', 'rb')}
+    global url
+    check_url = url + '/check/1'
+
+    assert requests.post(check_url, files = files).status_code == 200
+
+
+  
