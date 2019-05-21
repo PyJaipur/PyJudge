962dc8cfec9981e82766167140e08bc6185c833d
Rishabh Kalakoti <rishabh.kalakoti@gmail.com>
2019-05-21 12:24:31 +0530

    database completed.

--- server.py
+++ server.py
@@ -13,25 +13,6 @@ question_dir = "files/questions"
 db = SqliteDatabase(DATABASE_NAME)
 
 
-class Contest(Model):
-    code = CharField(unique=True)
-    description = CharField()
-    start_time = DateTimeField()
-    end_time = DateTimeField()
-
-    class Meta:
-        database = db
-
-
-class ContestProblems(Model):
-    contest = CharField()
-    question = IntegerField()
-
-    class Meta:
-        database = db
-        indexes = ((("contest", "question"), True),)
-
-
 class User(Model):
     username = CharField(unique=True)
     password = CharField()
@@ -42,60 +23,99 @@ class User(Model):
 
 class Session(Model):
     id = CharField(unique=True)
-    username = CharField()
+    user = ForeignKeyField(User)
 
     class Meta:
         database = db
 
 
+class Contest(Model):
+    code = CharField(unique=True)
+    description = CharField()
+    start_time = DateTimeField()
+    end_time = DateTimeField()
+
+    class Meta:
+        database = db
+
+
+class Question(Model):
+    q_no = IntegerField(unique=True)
+    author = ForeignKeyField(User)
+
+    class Meta:
+        database = db
+
+
+class ContestProblems(Model):
+    contest = ForeignKeyField(Contest)
+    question = ForeignKeyField(Question)
+
+    class Meta:
+        database = db
+        indexes = ((("contest", "question"), True),)
+
+
 class Submission(Model):
-    username = CharField()
+    user = ForeignKeyField(User)
     time = DateTimeField()
-    question = IntegerField()
-    contest = CharField()
+    question = ForeignKeyField(Question)
+    contest = ForeignKeyField(Contest)
     is_correct = BooleanField()
 
     class Meta:
         database = db
-        indexes = ((("username", "time"), True),)
+        indexes = ((("user", "time"), True),)
 
 
 db.connect()
-db.create_tables([User, Session, Submission, ContestProblems, Contest])
+db.create_tables([User, Session, Submission, ContestProblems, Contest, Question])
+
 
 # dummy contest data
-Contest.get_or_create(
+practiceContest = Contest.get_or_create(
     code="PRACTICE",
     description="practice questions",
     start_time=datetime.datetime(day=1, month=1, year=1),
     end_time=datetime.datetime(day=1, month=1, year=9999),
 )
-Contest.get_or_create(
+pastContest = Contest.get_or_create(
     code="PASTCONTEST",
     description="somewhere in the past",
     start_time=datetime.datetime(day=1, month=11, year=2018),
     end_time=datetime.datetime(day=1, month=12, year=2018),
 )
-Contest.get_or_create(
+ongoingContest = Contest.get_or_create(
     code="ONGOINGCONTEST",
     description="somewhere in the present",
     start_time=datetime.datetime(day=1, month=4, year=2019),
     end_time=datetime.datetime(day=1, month=6, year=2019),
 )
-Contest.get_or_create(
+futureContest = Contest.get_or_create(
     code="FUTURECONTEST",
     description="somewhere in the future",
     start_time=datetime.datetime(day=1, month=1, year=2020),
     end_time=datetime.datetime(day=1, month=10, year=2020),
 )
-ContestProblems.get_or_create(contest="PRACTICE", question=1)
-ContestProblems.get_or_create(contest="PRACTICE", question=2)
-ContestProblems.get_or_create(contest="PASTCONTEST", question=1)
-ContestProblems.get_or_create(contest="PASTCONTEST", question=2)
-ContestProblems.get_or_create(contest="ONGOINGCONTEST", question=3)
-ContestProblems.get_or_create(contest="ONGOINGCONTEST", question=4)
-ContestProblems.get_or_create(contest="FUTURECONTEST", question=5)
-ContestProblems.get_or_create(contest="FUTURECONTEST", question=6)
+
+test = User.get_or_create(username="test", password="test")
+
+q1 = Question.get_or_create(q_no=1, author=test[0])
+q2 = Question.get_or_create(q_no=2, author=test[0])
+q3 = Question.get_or_create(q_no=3, author=test[0])
+q4 = Question.get_or_create(q_no=4, author=test[0])
+q5 = Question.get_or_create(q_no=5, author=test[0])
+q6 = Question.get_or_create(q_no=6, author=test[0])
+
+
+ContestProblems.get_or_create(contest=practiceContest[0], question=q1[0])
+ContestProblems.get_or_create(contest=practiceContest[0], question=q2[0])
+ContestProblems.get_or_create(contest=pastContest[0], question=q1[0])
+ContestProblems.get_or_create(contest=pastContest[0], question=q2[0])
+ContestProblems.get_or_create(contest=ongoingContest[0], question=q3[0])
+ContestProblems.get_or_create(contest=ongoingContest[0], question=q4[0])
+ContestProblems.get_or_create(contest=futureContest[0], question=q5[0])
+ContestProblems.get_or_create(contest=futureContest[0], question=q6[0])
 
 
 def login_required(function):
@@ -131,13 +151,12 @@ def dashboard():
 def question(code, number):
     if (
         not ContestProblems.select()
-        .where(
-            (ContestProblems.contest == code)
-            & (ContestProblems.question == int(number))
-        )
+        .where((Contest.code == code) & (Question.q_no == int(number)))
+        .join(Contest, on=(ContestProblems.contest == Contest.id))
+        .join(Question, on=(ContestProblems.question == Question.q_no))
         .exists()
     ):
-        return bottle.abort(404, 'no such contest')
+        return bottle.abort(404, "no such contest problem")
     contest = Contest.get(Contest.code == code)
     if contest.start_time > datetime.datetime.now():
         return "The contest had not started yet."
@@ -152,11 +171,12 @@ def question(code, number):
 @login_required
 def contest(code):
     if not Contest.select().where(Contest.code == code).exists():
-        return bottle.abort(404, 'no such contest')
+        return bottle.abort(404, "no such contest")
     contest = Contest.get(Contest.code == code)
     questions = (
-        ContestProblems.select(ContestProblems.question)
-        .where(ContestProblems.contest == code)
+        ContestProblems.select(Question.q_no)
+        .where(ContestProblems.contest == contest)
+        .join(Question, on=(ContestProblems.question == Question.q_no))
         .tuples()
     )
     return bottle.template("contest.html", contest=contest, questions=list(questions))
@@ -175,12 +195,13 @@ def server_static(filepath):
 @app.get("/ranking/<code>")
 def contest_ranking(code):
     order = (
-        Submission.select(
-            Submission.username, fn.count(Submission.question).alias("score")
-        )
-        .where((Submission.is_correct == True) & (Submission.contest == code))
-        .group_by(Submission.username)
-        .order_by(fn.count(Submission.question).desc())
+        Submission.select(User.username, fn.count(Question.q_no).alias("score"))
+        .where((Submission.is_correct == True) & (Contest.code == code))
+        .join(User, on=(Submission.user == User.id))
+        .join(Question, on=(Submission.question == Question.q_no))
+        .join(Contest, on=(Submission.contest == Contest.id))
+        .group_by(Submission.user)
+        .order_by(fn.count(Question.q_no).desc())
     )
     order = list(order.tuples())
     order = [
@@ -192,12 +213,13 @@ def contest_ranking(code):
 @app.get("/ranking")
 def rankings():
     order = (
-        Submission.select(
-            Submission.username, fn.count(Submission.question).alias("score")
-        )
+        Submission.select(User.username, fn.count(Question.q_no).alias("score"))
         .where(Submission.is_correct == True)
-        .group_by(Submission.username)
-        .order_by(fn.count(Submission.question).desc())
+        .join(User, on=(Submission.user == User.id))
+        .switch()
+        .join(Question, on=(Submission.question == Question.q_no))
+        .group_by(Submission.user)
+        .order_by(fn.count(Question.q_no).desc())
     )
     order = list(order.tuples())
     order = [
@@ -224,7 +246,7 @@ def createSession(username):
         expires=datetime.datetime.now() + datetime.timedelta(days=30),
     )
     try:
-        Session.create(id=session_id, username=username)
+        Session.create(id=session_id, user=User.get(User.username == username))
     except IntegrityError:
         return abort("Error! Please try again.")
     return bottle.redirect("/dashboard")
@@ -268,14 +290,15 @@ def logout():
 def file_upload(code, number):
     if (
         not ContestProblems.select()
-        .where(
-            (ContestProblems.contest == code)
-            & (ContestProblems.question == int(number))
-        )
+        .where((Contest.code == code) & (Question.q_no == int(number)))
+        .join(Contest, on=(ContestProblems.contest == Contest.id))
+        .join(Question, on=(ContestProblems.question == Question.q_no))
         .exists()
     ):
-        return error404(404)
-    username = Session.get(Session.id == bottle.request.get_cookie("s_id")).username
+        return bottle.abort(404, "no such contest problem")
+    username = Session.get(
+        Session.id == bottle.request.get_cookie("s_id")
+    ).user.username
     time = datetime.datetime.now()
     uploaded = bottle.request.files.get("upload").file.read()
     with open(os.path.join(question_dir, number, "output.txt"), "rb") as fl:
@@ -285,7 +308,11 @@ def file_upload(code, number):
     ans = uploaded == expected
     try:
         Submission.create(
-            username=username, question=number, time=time, contest=code, is_correct=ans
+            user=User.get(User.username == username),
+            question=Question.get(Question.q_no == number),
+            time=time,
+            contest=Contest.get(Contest.code == code),
+            is_correct=ans,
         )
     except:
         abort("Error in inserting submission to database.")
