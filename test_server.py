be365ea1e8d6fb5827d6072150477ca451a1eb56
Rishabh Kalakoti <rishabh.kalakoti@gmail.com>
2019-05-23 17:09:24 +0530

    comments

--- test_server.py
+++ test_server.py
@@ -5,24 +5,23 @@ import requests
 
 def test_demo():
     assert 1==1
-"""
-url = 'http://localhost:8080'
 
-def test_question():
-    r = requests.get(url+'/question/1')
-    assert r.status_code == 200
+#url = 'http://localhost:8080'
 
-def test_download():
+#def test_question():
+#    r = requests.get(url+'/question/1')
+#    assert r.status_code == 200
+
+#def test_download():
     #time = datetime.datetime.now()
 
     # type(uploaded) == <class 'bytes'>
     # uploaded outputs by user
-    assert requests.get(url+'/question/1/inputs.txt').status_code == 200
+#    assert requests.get(url+'/question/1/inputs.txt').status_code == 200
 
-def test_file_upload():
-    files= {'upload' : open('files/questions/1/output.txt', 'rb')}
-    global url
-    check_url = url + '/check/1'
+#def test_file_upload():
+#    files= {'upload' : open('files/questions/1/output.txt', 'rb')}
+#    global url
+#    check_url = url + '/check/1'
 
-    assert requests.post(check_url, files = files).status_code == 200
-"""
+#    assert requests.post(check_url, files = files).status_code == 200
