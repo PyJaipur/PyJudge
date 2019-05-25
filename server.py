11c89f0486280a2d3305423803405e9ff506de06
rahulVarma <rahulsvarm@gmail.com>
2019-05-25 18:11:29 +0530

    User Submission History and User Stats (#131)
    * User Submission History and User Stats
    
    User Submission History and User Stats
    
    * Updated to include new db changes and user specific data
    
    Updated to include new db changes and user specific data
    
    * Updated Code
    
    Updated Code
    
    * Updated as per comments
    
    Updated to check user session cookie, get count from already read database and directly convert to list
    
    * Updated to handle Session changes
    
    Updated to handle statsitics even if the session changes
    
    * Updated as per @rishabhKalakoti and @ theSage21 comments
    
    Updated as per @rishabhKalakoti and @ theSage21 comments
    
    * Formatted code
    
    Formatted code

--- server.py
+++ server.py
@@ -73,7 +73,6 @@ class Submission(Model):
 db.connect()
 db.create_tables([User, Session, Submission, ContestProblems, Contest, Question])
 
-
 # dummy contest data
 practiceContest = Contest.get_or_create(
     code="PRACTICE",
@@ -123,6 +122,8 @@ def login_required(function):
     def login_redirect(*args, **kwargs):
         if not logggedIn():
             return bottle.template("home.html", message="Login required.")
+        me = Session.get(Session.token == bottle.request.get_cookie("s_id"))
+        bottle.request.session = me
         return function(*args, **kwargs)
 
     return login_redirect
@@ -145,6 +146,33 @@ def home():
 def dashboard():
     contests = Contest.select().order_by(Contest.start_time)
     return bottle.template("dashboard.html", contests=contests)
+
+
+@app.get("/stats")
+@login_required
+def statistics():
+    sub_history = (
+        Submission.select(
+            Contest.code,
+            ContestProblems.question,
+            Submission.time,
+            Submission.is_correct,
+        )
+        .where(Submission.user == bottle.request.session.user)
+        .join(ContestProblems, on=(Submission.contestProblem == ContestProblems.id))
+        .switch()
+        .join(Contest, on=(ContestProblems.contest == Contest.id))
+        .order_by(Submission.time.desc())
+        .dicts()
+    )
+    sub_stats_total = len(sub_history)
+    sub_stats_correct = len([sub_history for sub in sub_history if sub["is_correct"]])
+    return bottle.template(
+        "stats.html",
+        sub_history=sub_history,
+        sub_stats_correct=sub_stats_correct,
+        sub_stats_total=sub_stats_total,
+    )
 
 
 @app.get("/contest/<code>/<number>")
@@ -231,6 +259,7 @@ def rankings():
         (username, score, rank) for rank, (username, score) in enumerate(order, start=1)
     ]
     return bottle.template("rankings.html", people=order)
+
 
 def logggedIn():
     if not bottle.request.get_cookie("s_id"):
