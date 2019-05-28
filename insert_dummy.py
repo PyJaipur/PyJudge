97cfcad2f165eb61cf19dd9357bfbe158bc81931
Raul9595 <rahulsvarm@gmail.com>
2019-05-28 21:39:07 +0530

    Updated insert_dummy.py file
    Updated file to populate test data without database call and directly through POST method

--- insert_dummy.py
+++ insert_dummy.py
@@ -1,126 +1,70 @@
+import requests
 import datetime
-from peewee import *
-
-DATABASE_NAME = "data.db"
-db = SqliteDatabase(DATABASE_NAME)
+import os
 
 
-class User(Model):
-    username = CharField(unique=True)
-    password = CharField()
+path = os.path.abspath(__file__)
+dir_path = os.path.dirname(path)
+question_dir = "files/questions"
 
-    class Meta:
-        database = db
+with requests.Session() as session:
+    register = session.post(
+        "http://localhost:8080/register", data={"username": "test", "password": "test"}
+    )
 
+    login = session.post(
+        "http://localhost:8080/login", data={"username": "test", "password": "test"}
+    )
 
-class Contest(Model):
-    code = CharField(unique=True)
-    description = CharField()
-    start_time = DateTimeField()
-    end_time = DateTimeField()
+    for i in range(1, 7, 1):
+        Question1 = session.post(
+            "http://localhost:8080/questionInput",
+            files={
+                "question": open(
+                    os.path.join(question_dir, str(i), "inputs.txt"), "rb"
+                ),
+                "answer": open(os.path.join(question_dir, str(i), "output.txt"), "rb"),
+            },
+            data={"statement": str(i)},
+        )
 
-    class Meta:
-        database = db
-
-
-class Question(Model):
-    test_case_input = TextField()
-    test_case_output = TextField()
-    question_statement = CharField()
-    author = ForeignKeyField(User)
-    created_date_time = DateTimeField(default=datetime.datetime.now)
-
-    class Meta:
-        database = db
-
-
-class ContestProblems(Model):
-    contest = ForeignKeyField(Contest, backref="questions")
-    question = ForeignKeyField(Question)
-
-    class Meta:
-        database = db
-        indexes = ((("contest", "question"), True),)
-
-
-db.connect()
-
-# dummy contest data
-practiceContest = Contest.get_or_create(
-    code="PRACTICE",
-    description="practice questions",
-    start_time=datetime.datetime(day=1, month=1, year=1),
-    end_time=datetime.datetime(day=1, month=1, year=9999),
-)
-pastContest = Contest.get_or_create(
-    code="PASTCONTEST",
-    description="somewhere in the past",
-    start_time=datetime.datetime(day=1, month=11, year=2018),
-    end_time=datetime.datetime(day=1, month=12, year=2018),
-)
-ongoingContest = Contest.get_or_create(
-    code="ONGOINGCONTEST",
-    description="somewhere in the present",
-    start_time=datetime.datetime(day=1, month=4, year=2019),
-    end_time=datetime.datetime(day=1, month=6, year=2019),
-)
-futureContest = Contest.get_or_create(
-    code="FUTURECONTEST",
-    description="somewhere in the future",
-    start_time=datetime.datetime(day=1, month=1, year=2020),
-    end_time=datetime.datetime(day=1, month=10, year=2020),
-)
-
-test = User.get_or_create(username="test", password="test")
-
-q1 = Question.get_or_create(
-    test_case_input="1",
-    test_case_output="1",
-    question_statement="1",
-    author=test[0],
-    created_date_time=datetime.datetime.now(),
-)
-q2 = Question.get_or_create(
-    test_case_input="2",
-    test_case_output="2",
-    question_statement="1",
-    author=test[0],
-    created_date_time=datetime.datetime.now(),
-)
-q3 = Question.get_or_create(
-    test_case_input="3",
-    test_case_output="3",
-    question_statement="1",
-    author=test[0],
-    created_date_time=datetime.datetime.now(),
-)
-q4 = Question.get_or_create(
-    test_case_input="4",
-    test_case_output="4",
-    question_statement="1",
-    author=test[0],
-    created_date_time=datetime.datetime.now(),
-)
-q5 = Question.get_or_create(
-    test_case_input="5",
-    test_case_output="5",
-    question_statement="1",
-    author=test[0],
-    created_date_time=datetime.datetime.now(),
-)
-q6 = Question.get_or_create(
-    test_case_input="6",
-    test_case_output="6",
-    question_statement="1",
-    author=test[0],
-    created_date_time=datetime.datetime.now(),
-)
-
-ContestProblems.get_or_create(contest=practiceContest[0], question=q1[0])
-ContestProblems.get_or_create(contest=practiceContest[0], question=q2[0])
-ContestProblems.get_or_create(contest=pastContest[0], question=q1[0])
-ContestProblems.get_or_create(contest=pastContest[0], question=q2[0])
-ContestProblems.get_or_create(contest=ongoingContest[0], question=q3[0])
-ContestProblems.get_or_create(contest=ongoingContest[0], question=q4[0])
-ContestProblems.get_or_create(contest=futureContest[0], question=q5[0])
-ContestProblems.get_or_create(contest=futureContest[0], question=q6[0])
+    Contest1 = session.post(
+        "http://localhost:8080/contestInput",
+        data={
+            "code": "PRACTICE",
+            "description": "practice questions",
+            "start_time": datetime.datetime(day=1, month=1, year=1),
+            "end_time": datetime.datetime(day=1, month=1, year=9999),
+            "selection": [1, 2],
+        },
+    )
+    Contest2 = session.post(
+        "http://localhost:8080/contestInput",
+        data={
+            "code": "PASTCONTEST",
+            "description": "somewhere in the past",
+            "start_time": datetime.datetime(day=1, month=11, year=2018),
+            "end_time": datetime.datetime(day=1, month=12, year=2018),
+            "selection": [1, 2],
+        },
+    )
+    Contest3 = session.post(
+        "http://localhost:8080/contestInput",
+        data={
+            "code": "ONGOINGCONTEST",
+            "description": "somewhere in the present",
+            "start_time": datetime.datetime(day=1, month=4, year=2019),
+            "end_time": datetime.datetime(day=1, month=6, year=2019),
+            "selection": [3, 4],
+        },
+    )
+    Contest4 = session.post(
+        "http://localhost:8080/contestInput",
+        data={
+            "code": "FUTURECONTEST",
+            "description": "somewhere in the future",
+            "start_time": datetime.datetime(day=1, month=1, year=2020),
+            "end_time": datetime.datetime(day=1, month=10, year=2020),
+            "selection": [5, 6],
+        },
+    )
